11b44bee3a610896bae9d8bce4aa772b85f059cd
Cody Mumford <32171900+HeartofQuantum@users.noreply.github.com>
2018-04-07 15:54:57 -0500

    created ClaimsAuthorizationAttribute.cs with properties and OnAuthorizationAsync Method

---
+++ MusicFlashApi/Infrastructure/ClaimsAuthorizationAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace MusicFlashApi.Infrastructure
+{
+    public class ClaimsAuthorizationAttribute : AuthorizationFilterAttribute 
+    {
+
+        public string ClaimType { get; set;}
+        public string ClaimValue{ get; set; }
+
+        public override Task OnAuthorizationAsync(HttpActionContext actionContext, System.Threading.CancellationToken cancellationToken)
+        {
+            var prinicipal = actionContext.RequestContext.Principal as ClaimsPrincipal;
+
+            if (!prinicipal.Identity.IsAuthenticated)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+
+            if (!(prinicipal.HasClaim(x => x.Type == ClaimType && x.Value == ClaimValue)))
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                return Task.FromResult<object>(null);
+            }
+
+            //User is Authorizaed, complete execution
+            return Task.FromResult<object>(null);
+        }
+
+    }
+}
